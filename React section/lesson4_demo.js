6206729477b2b77771c97643d4150dd9b81f14af
Shrilakshmi-NK <ininidinji@gmail.com>
2026-03-19 23:59:23 +0530

    add react section

---
+++ React section/lesson4_demo.js
@@ -0,0 +1,18 @@
+//For loop
+for(let i=0; i <= 10; i++){
+console.log(i);
+}
+
+// while loop
+let count= 0;
+while (count< 10){
+console.log(count);
+count++;
+}
+
+// do/while loop
+let count1 = 0;
+do{
+  count1++;
+  console.log("count is "+ count1);
+}while(count1<10);
