6ff644f86696940b60354c98321844afca061ad3
Shrilakshmi-NK <ininidinji@gmail.com>
2026-03-22 12:33:12 +0530

    lesson 5 practice

--- React_section/lesson5_demo.js
+++ React_section/lesson5_demo.js
@@ -17,3 +17,29 @@ sayName() {
   console.log(this.name);
   }
 };
+
+
+/*When a property is explicitly added to an object, Configurable, Enumerable and Writable are set to true while the Value attribute is set to the assigned value.*/
+
+// Example :
+
+let person = {
+  name: "Nicholas"
+};
+
+
+//Object.defineProperty() method
+let person = {};
+Object.defineProperty(person, "name", {
+  writable: false,
+  value: "Nicholas"
+});
+console.log(person.name); // "Nicholas"
+person.name = "Greg";
+console.log(person.name); // "Nicholas"
+
+let customer={
+  first_name:"Naya",
+  lasta_name:"Martin",
+  phone:"0857162863"
+};
