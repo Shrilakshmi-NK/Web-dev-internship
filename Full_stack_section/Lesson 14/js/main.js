38b24244d02f42778ee097a89522b20853caf560
Shrilakshmi-NK <ininidinji@gmail.com>
2026-04-12 16:05:20 +0530

    add js folder

---
+++ Full_stack_section/Lesson 14/js/main.js
@@ -0,0 +1,27 @@
+var modal = document.querySelector('.modal');
+modal.style.display="none";
+
+var loginForm = document.querySelector("#login-form");
+
+loginForm.addEventListener('submit', function(e)
+{
+  e.preventDefault();
+})
+
+function validateLogin(){
+  var unameField = document.querySelector("#uname");
+  var pwdField = document.querySelector("#pwd");
+
+  if(unameField.value!="Admin" || pwdField.value!="123456"){
+    modal.style.display="block";
+
+  }
+  else if(unameField.value=="Admin" || pwdField.value=="123456"){
+    alert("You are now logged in!")
+    window.location.href= "index.html"
+  }
+}
+
+function dismissModal(){
+  modal.style.display="none";
+}
