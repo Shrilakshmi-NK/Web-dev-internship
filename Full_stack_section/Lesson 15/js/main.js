208b6455d9bc58d806b2239898d4c527041a0c86
Shrilakshmi-NK <ininidinji@gmail.com>
2026-04-14 17:40:23 +0530

    add js file

---
+++ Full_stack_section/Lesson 15/js/main.js
@@ -0,0 +1,9 @@
+var employeeListHolder = document.querySelector(".employee-list");
+var interviewListHolder = document.querySelector(".interviews");
+var addEmployee = document.querySelector("#employeeModal");
+var addInterviewModal = document.querySelector("#interviewModal");
+
+addInterviewModal.style.display="none";
+addEmployee.style.display="none";
+
+
