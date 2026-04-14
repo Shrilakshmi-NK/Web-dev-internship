356d2f79ff36a80bf844922dc355266a4849276a
Shrilakshmi-NK <ininidinji@gmail.com>
2026-04-14 16:43:50 +0530

    add lesson 8

---
+++ Full_stack_section/Lesson 18/practice.php
@@ -0,0 +1,20 @@
+<?php
+echo "This is my first line ever in PHP script" . "<br>";
+echo 15 ."<br>"; 
+
+
+echo "This is my first line ever in PHP script" . "<br>";
+echo 24 . "<br>";
+
+$myName = "Mace";
+echo $myName;
+echo "<br>";
+$num= 26;
+echo $num;
+echo "<br>";
+
+$num1=6;
+$num2=4;
+$result=$num1+$num2;
+echo $result;
+?>
