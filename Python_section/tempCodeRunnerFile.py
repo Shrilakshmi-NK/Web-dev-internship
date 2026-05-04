b5ac462b2362069fed9466b711bc5da2a422abaf
Shrilakshmi-NK <ininidinji@gmail.com>
2026-05-04 16:44:39 +0530

    add csv and temp files

---
+++ Python_section/tempCodeRunnerFile.py
@@ -0,0 +1,27 @@
+pay_load3 = np.array(
+# [
+# [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+# [[10, 11, 12], [13, 14, 15], [16, 17, 18]],
+# [[19, 20, 21], [22, 23, 24], [25, 26, 27]]
+# ]
+# )
+
+# print(pay_load3.ndim)
+
+# a0= np.arange(6)
+# print(a0)
+
+# b0=a.reshape(2,3)
+# print(b0)
+
+# arr0 = np.arange(1,15)
+# arr0 = np.arange(10,50,5) #step value is 5
+# print(arr0)
+
+# a= np.random.rand(7)
+# b= np.random.rand(2,4) #2- makes it a 2D array
+# print(a)
+
+# a = np.array([2,4,6,8])
+# b=np.sum(a)
+# print(b)
